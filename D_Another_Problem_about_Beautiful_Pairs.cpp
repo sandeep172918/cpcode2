f00e6393ce75d1841d7111d1b81043a3890b2857
hellowcoder <iitianias@gmail.com>
2026-02-12 10:51:17 +0530

    div2_better

---
+++ D_Another_Problem_about_Beautiful_Pairs.cpp
@@ -0,0 +1,99 @@
+
+//Author: sandeep172918
+//Date: 2026-02-11 21:30
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define lli long long int
+#define fr(i,n) for(lli i=0;i<n;i++)
+#define frs(i,a,b) for(lli i=a;i<=b;i++)
+#define rfr(i,b,a) for(lli i=b;i>=a;i--)
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define pr pair<lli,lli>
+#define vll vector<lli>
+#define vbl vector<bool>
+#define vpr vector<pr>
+#define vvll vector<vector<lli>>
+#define get(v,n) vll v(n);fr(i,n)cin>>v[i]
+#define ff first
+#define ss second
+#define tr true
+#define fs false
+#define bitc(x) __builtin_popcountll(x)
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define psb(a) push_back(a)
+#define ppb pop_back()
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sq(x) sqrtl(x)
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define no1 cout<<"-1\n"
+#define nl cout<<"\n"
+#define out(v) fr(i,v.size())cout<<v[i]<<" ";nl
+#define srtp(v) sort(all(v),[](const pr& a,const pr& b){if(a.ff== b.ff)return a.ss>b.ss; return a.ff<b.ff;});
+using namespace std;
+const int MOD=1e9+7;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+void solve(){
+lli n,k;cin>>n;
+get(v,n);
+vvll p(n);
+fr(i,n){
+    if(v[i]<n)p[v[i]].psb(i);
+}
+lli ans=0;
+for(lli x=1;x*x<=n;x++){
+    if(p[x].empty())continue;
+    for(lli y=x;x*y<n;y++){
+        k=x*y;
+        if(x==y){
+           for(auto &it:p[x]){
+            if(binary_search(all(p[y]),it+k)){
+                ans++;
+            }
+           }
+        }else{
+           if(p[x].size()<p[y].size()){
+             for(auto &it:p[x]){
+            if(binary_search(all(p[y]),it+k)){
+                ans++;
+            }
+            if(binary_search(all(p[y]),it-k)){
+                ans++;
+            }
+           }
+           }else{
+            for(auto &it:p[y]){
+            if(binary_search(all(p[x]),it+k)){
+                ans++;
+            }
+            if(binary_search(all(p[x]),it-k)){
+                ans++;
+            }
+           }
+           }
+        }
+    }
+}
+
+cout<<ans<<'\n';
+
+}
+
+int32_t main(){
+fastio;
+lli test=1;
+cin>>test;
+while(test--){
+solve();
+}
+}
+
