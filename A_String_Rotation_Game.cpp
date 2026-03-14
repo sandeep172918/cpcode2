13b6d2d21cfaac2606f3ff5d3a39e7e29a82d599
hellowcoder <iitianias@gmail.com>
2026-03-14 22:45:28 +0530

    grinded

--- A_String_Rotation_Game.cpp
+++ A_String_Rotation_Game.cpp
@@ -1,5 +1,5 @@
 //Author:coding_with_alzheimer
-//Date: 2026-03-14 15:25
+//Date: 2026-03-14 20:01
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -29,8 +29,8 @@
 #define rall(v) v.rbegin(),v.rend()
 #define sq(x) sqrtl(x)
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
-#define yes cout<<"Alice\n"
-#define no cout<<"Bob\n"
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
 #define no1 cout<<"-1\n"
 #define nl cout<<"\n"
 #define out(v) fr(i,v.size())cout<<v[i]<<" ";nl
@@ -76,39 +76,19 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 //BSDK math snippet hai
 void solve(){
-lli n,k;cin>>n>>k;
-get(a,n);
-get(b,k);
-srt(a);
-lli l=1;
-lli x=mxe(b);
-fr(i,n){
-    l=(l/__gcd(l,a[i]))*a[i];
-    if(l>x){
-        l=x+1;
-        break;
+lli n,k;;
+cin>>n;
+string s;cin>>s;
+s+=s;
+lli ans=0;
+fr(i,n+1){
+    lli curr=1;
+    frs(j,i+1,i+n-1){
+       if(s[j]!=s[j-1])curr++;
     }
+    ans=max(ans,curr);
 }
-vll div(x+10,0);
-fr(i,n){
-    if(a[i]<=x && !div[a[i]] ){
-      for(lli j=a[i];j<=x;j+=a[i]){
-         div[j]=1;
-    }
-   }
-}
-lli aa=0,bb=0,m=0;
-fr(i,k){
-  bool alice=div[b[i]];
-  bool bob=(b[i]%l !=0);
-  if(alice && !bob)aa++;
-  else if(!alice && bob)bb++;
-  else if(alice && bob)m++;
-
-}
-if((aa+(m%2))>bb)yes;
-else no;
-
+cout<<ans<<'\n';
 }
 
 int32_t main(){
--- C_All_in_one_Gun.cpp
+++ C_All_in_one_Gun.cpp
@@ -1,5 +1,5 @@
 //Author:coding_with_alzheimer
-//Date: 2026-03-14 15:25
+//Date: 2026-03-14 20:27
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -29,8 +29,8 @@
 #define rall(v) v.rbegin(),v.rend()
 #define sq(x) sqrtl(x)
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
-#define yes cout<<"Alice\n"
-#define no cout<<"Bob\n"
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
 #define no1 cout<<"-1\n"
 #define nl cout<<"\n"
 #define out(v) fr(i,v.size())cout<<v[i]<<" ";nl
@@ -76,38 +76,46 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 //BSDK math snippet hai
 void solve(){
-lli n,k;cin>>n>>k;
-get(a,n);
-get(b,k);
-srt(a);
-lli l=1;
-lli x=mxe(b);
-fr(i,n){
-    l=(l/__gcd(l,a[i]))*a[i];
-    if(l>x){
-        l=x+1;
-        break;
-    }
+lli n,h,k;cin>>n>>h>>k;
+get(v,n);
+vll suff(n+1);
+rfr(i,n-1,0){
+    suff[i]=max(suff[i+1],v[i]);
 }
-vll div(x+10,0);
-fr(i,n){
-    if(a[i]<=x && !div[a[i]] ){
-      for(lli j=a[i];j<=x;j+=a[i]){
-         div[j]=1;
-    }
-   }
-}
-lli aa=0,bb=0,m=0;
-fr(i,k){
-  bool alice=div[b[i]];
-  bool bob=(b[i]%l !=0);
-  if(alice && !bob)aa++;
-  else if(!alice && bob)bb++;
-  else if(alice && bob)m++;
+lli sum=accumulate(all(v),0ll);
 
+lli p=h/sum;
+lli rem=h%sum;
+lli tot=p*n+(p-1)*k;
+tot+=(rem>0)*k;
+if(rem==0){
+    cout<<tot<<'\n';
+    return;
 }
-if((aa+(m%2))>bb)yes;
-else no;
+// cout<<tot<<'\n';
+h-=(p*sum);
+ //cout<<h<<'\n';
+lli curr=0;
+
+vll pre(n,1e13);
+pre[0]=v[0];
+frs(i,1,n-1){
+  pre[i]=min(pre[i-1],v[i]);
+}
+
+fr(i,n){
+  curr+=v[i];  
+ lli op1=curr;
+ lli op2=curr-pre[i]+suff[i+1];
+ lli c=max(op1,op2);
+ // cout<<op1<<' '<<op2<<'\n';
+//  cout<<c<<'\n';
+ if(h<=c){
+     cout<<tot+i+1<<'\n';
+    // cout<<"hi\n";
+    return;
+ }
+}
 
 }
 
