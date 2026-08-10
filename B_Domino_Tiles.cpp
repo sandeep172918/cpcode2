38f0ea1cbbcb8705ba288316e508a6957e0a1644
hellowcoder <iitianias@gmail.com>
2026-08-10 18:34:47 +0530

    contest sol

---
+++ B_Domino_Tiles.cpp
@@ -0,0 +1,91 @@
+//Author:coding_with_alzheimer
+//Date: 2026-08-09 20:15
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define lli long long int
+#define fr(i,n) for(lli i=0;i<n;i++)
+#define frs(i,a,b) for(lli i=a;i<=b;i++)
+#define rfr(i,b,a) for(lli i=b;i>=a;i--)
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define pr pair<lli,lli>
+#define vll vector<lli>
+#define vbl vector<bool>
+#define vpr vector<pr>
+#define vvll vector<vector<lli>>
+#define get(v,n) vll v(n);fr(i,n)cin>>v[i]
+#define ff first
+#define ss second
+#define tr true
+#define fs false
+#define bitc(x) __builtin_popcountll(x)
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define psb(a) push_back(a)
+#define ppb pop_back()
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sq(x) sqrtl(x)
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define no1 cout<<"-1\n"
+#define nl cout<<"\n"
+#define out(v) fr(i,v.size())cout<<v[i]<<" ";nl
+#define srtp(v) sort(all(v),[](const pr& a,const pr& b){if(a.ff== b.ff)return a.ss>b.ss; return a.ff<b.ff;});
+using namespace std;
+const int MOD=998244353;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+void solve(){
+lli n=0,k=0;string s;
+cin>>n;
+cin>>s;
+vll v(n);
+vll vv(n);
+fr(i,n){
+    if(i&1)v[i]=1;
+    else vv[i]=1;
+}
+bool a=1,b=1;
+lli j=0;
+for(lli i=0;i<n;i+=2){
+   if(s[i]!='?'){
+    k=s[i]-'0';
+    if(k!=v[j])a=0;
+    if(k!=vv[j])b=0;
+   }
+   j++;
+}
+j=0;
+lli ans=a+b;
+a=1;
+b=1;
+for(lli i=1;i<n;i+=2){
+   if(s[i]!='?'){
+    k=s[i]-'0';
+    if(k!=v[j])a=0;
+    if(k!=vv[j])b=0;
+   }
+   j++;
+}
+ans*=(a+b);
+cout<<ans<<'\n';
+
+
+}
+
+int32_t main(){
+fastio;
+lli test=1;
+cin>>test;
+while(test--){
+solve();
+}
+}
